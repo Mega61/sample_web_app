6dfe81a90f289d61e33949fa18204126b235e335
Juan E Daza <jdazap@choucairtesting.com>
2023-10-31 13:38:05 -0500

    Initial upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,143 @@
-import Image from 'next/image'
+"use client";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [cityName, setCityName] = useState('');
+  const [unit, setUnit] = useState('');
+  const [lat, setLatitude] = useState('');
+  const [lng, setLongitute] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [locationData, setLocationData] = useState(null);
+
+  const handleSubmitWeather = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.1:3000/weather', {
+        cityName: cityName,
+        unit: unit,
+      });
+      const data = response.data;
+      setWeatherData(data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  const handleSubmitLocation = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.2:4000/location', {
+        lat: lat,
+        lng: lng,
+      });
+      const data = response.data;
+      setLocationData(data);
+    } catch (error) {
+      console.error('Error fetching location data:', error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="container mx-auto p-4">
+      <section className="bg-white p-6 rounded-lg shadow mt-4">
+        <h2 className="text-xl text-black font-semibold mb-4">Weather App</h2>
+
+        <h3 className="text-lg text-black font-semibold">
+          A basic Weather app microservice
+        </h3>
+        <p>Enter your details to get weather Info </p>
+        <form onSubmit={handleSubmitWeather}>
+          <label className="text-black" htmlFor="cityInput">
+            City name:
+          </label>
+          <input
+            className="text-black"
+            type="text"
+            name="cityName"
+            placeholder="Enter name of your city"
+            id="cityInput"
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+          />
+          <label className="text-black" htmlFor="unitInput">
+            Unit:
+          </label>
+          <input
+            className="text-black"
+            type="text"
+            name="unit"
+            placeholder="Enter unit in metric"
+            id="unitInput"
+            value={unit}
+            onChange={(e) => setUnit(e.target.value)}
+          />
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-lg mt-2 hover:bg-green-600"
+            type="submit"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            Submit
+          </button>
+        </form>
+        {weatherData && (
+          <div>
+            <h4 className="text-black">Weather Information</h4>
+            <p className="text-black">Temperature: {(weatherData as any).temperature}°C</p>
+            <p className="text-black">Description: {(weatherData as any).weatherDes}</p>
+            <p className="text-black">City Name: {(weatherData as any).cityName}</p>
+            <img src={(weatherData as any).imageURL} alt="Weather Icon" />
+          </div>
+        )}
+      </section >
+      <section className="bg-white p-6 rounded-lg shadow mt-4">
+        <h2 className="text-xl text-black font-semibold mb-4">Location App</h2>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <h3 className="text-lg text-black font-semibold">
+          A basic Location app microservice
+        </h3>
+        <p>Enter your details to get Location Info based on longitute and latitude </p>
+        <form onSubmit={handleSubmitLocation}>
+          <label className="text-black" htmlFor="latInput">
+            Lat:
+          </label>
+          <input
+            className="text-black"
+            type="text"
+            name="lat"
+            placeholder="Enter latitude"
+            id="latInput"
+            value={lat}
+            onChange={(e) => setLatitude(e.target.value)}
+          />
+          <label className="text-black" htmlFor="lngInput">
+            lng:
+          </label>
+          <input
+            className="text-black"
+            type="text"
+            name="lng"
+            placeholder="Enter longitute"
+            id="lngInput"
+            value={lng}
+            onChange={(e) => setLongitute(e.target.value)}
+          />
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-lg mt-2 hover:bg-green-600"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+        {locationData && (
+          <div>
+            <h4 className="text-black">Locaton Information</h4>
+            <p className="text-black">CountryName: {(locationData as any).countryName}</p>
+            <p className="text-black">CityName: {(locationData as any).city}</p>
+          </div>
+        )}
+      </section >
+    </div >
+  );
 }
